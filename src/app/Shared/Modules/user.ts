cc979faec5aed3622d07fe17a71465129ca185a2
Colton Noble <w0843636@myscc.ca>
2024-09-18 14:32:29 -0400

    Starting Assignment 2

---
+++ src/app/Shared/Modules/user.ts
@@ -0,0 +1,8 @@
+export interface VideoGames {
+  price: number;
+  title: string;
+  genre: string;
+  yearOfRelease: number;
+  Owned: boolean
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VideoGames } from './Shared/Modules/user';
+import {NgForOf, NgIf, } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title:string = "Colton Noble's "
-  num:number = 8;
-  float:number = 3.4;
+
+
+  title = "Making a VideoGame!"
+  Games: VideoGames[] = [
+    { price: 79.99, title: "Tekken 8", genre: "Fighting Game", yearOfRelease: 2024, Owned: false },
+    { price: 60.99, title: "Elden Ring", genre: "Souls like", yearOfRelease: 2022, Owned: false },
+    { price: 79.99, title: "Call Of Duty", genre: "First Person Shooter", yearOfRelease: 2024, Owned: false },
+    { price: 40.99, title: "Street Fighter 6", genre: "Fighting game", yearOfRelease: 2024, Owned: false },
+    { price: 0, title: "Rocket League", genre: "Sports Game", yearOfRelease: 2015, Owned: false },
+    { price: 0, title: "Apex Legends", genre: "Battle royal", yearOfRelease: 2019, Owned: false }
+  ];
+
+  toggleOwned(game: VideoGames ): void {
+    game.Owned = !game.Owned;
+  }
+
 }
