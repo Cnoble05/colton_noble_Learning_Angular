49d33c91af8d95eca4352a3b940df1749a5e4241
Cnoble05 <w0843636@myscc.ca>
2024-09-24 23:58:20 -0400

    Changed package.json to 0.2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,14 @@ export class AppComponent {
 
 
   title = "Making a VideoGame!"
-  Games: VideoGames[] = [
-    { price: 79.99, title: "Tekken 8", genre: "Fighting Game", yearOfRelease: 2024, Owned: false },
-    { price: 60.99, title: "Elden Ring", genre: "Souls like", yearOfRelease: 2022, Owned: false },
-    { price: 79.99, title: "Call Of Duty", genre: "First Person Shooter", yearOfRelease: 2024, Owned: false },
-    { price: 40.99, title: "Street Fighter 6", genre: "Fighting game", yearOfRelease: 2024, Owned: false },
-    { price: 0, title: "Rocket League", genre: "Sports Game", yearOfRelease: 2015, Owned: false },
-    { price: 0, title: "Apex Legends", genre: "Battle royal", yearOfRelease: 2019, Owned: false }
-  ];
+
+    VideoGame1: VideoGames = { price: 79.99, title: "Tekken 8", genre: "Fighting Game", yearOfRelease: 2024, Owned: false };
+    VideoGame2: VideoGames ={ price: 60.99, title: "Elden Ring", genre: "Souls like", yearOfRelease: 2022, Owned: false };
+    VideoGame3: VideoGames ={ price: 79.99, title: "Call Of Duty", genre: "First Person Shooter", yearOfRelease: 2024, Owned: false };
+    VideoGame4: VideoGames ={ price: 40.99, title: "Street Fighter 6", genre: "Fighting game", yearOfRelease: 2024, Owned: false };
+
+    VideoGameList : VideoGames[] =
+      [this.VideoGame1,this.VideoGame2,this.VideoGame3,this.VideoGame4]
 
   toggleOwned(game: VideoGames ): void {
     game.Owned = !game.Owned;
