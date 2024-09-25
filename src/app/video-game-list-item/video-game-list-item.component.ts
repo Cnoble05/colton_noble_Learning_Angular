8cd12d5a7fe78f25059b0fabd79e586dabb5c1a2
Cnoble05 <w0843636@myscc.ca>
2024-09-25 00:02:27 -0400

    ng generate component1 and componet 2

---
+++ src/app/video-game-list-item/video-game-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-game-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-game-list-item.component.html',
+  styleUrl: './video-game-list-item.component.css'
+})
+export class VideoGameListItemComponent {
+
+}
---
+++ src/app/video-game-list/video-game-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-game-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-game-list.component.html',
+  styleUrl: './video-game-list.component.css'
+})
+export class VideoGameListComponent {
+
+}
