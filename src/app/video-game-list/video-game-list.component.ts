d6c845f9fed5be4892f9af65e410bae9d83d78ee
Cnoble05 <w0843636@myscc.ca>
2024-10-01 18:07:15 -0400

    Changed package.json to match current assignment - added new branch

--- src/app/video-game-list/video-game-list.component.ts
+++ src/app/video-game-list/video-game-list.component.ts
@@ -16,4 +16,10 @@ export class VideoGameListComponent {
   VideoGame2: VideoGames = { price: 60.99, title: "Elden Ring", genre: "Souls like", yearOfRelease: 2022, Owned: false };
   VideoGame3: VideoGames = { price: 79.99, title: "Call Of Duty", genre: "First Person Shooter", yearOfRelease: 2024, Owned: false };
   VideoGame4: VideoGames = { price: 40.99, title: "Street Fighter 6", genre: "Fighting game", yearOfRelease: 2024, Owned: false };
+
+  toggleOwned(game: VideoGames ): void {
+    game.Owned = !game.Owned;
+  }
 }
+
+
