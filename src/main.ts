b957ae253f441c806e58ca8d66a8f12ac5fcd312
Cnoble05 <w0843636@myscc.ca>
2024-11-15 20:52:48 -0500

    Created lazy loading in my main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import {provideRouter, Routes} from "@angular/router";
-import {VideoGameListComponent} from "./app/video-game-list/video-game-list.component";
-import {VideoGameListItemComponent} from "./app/video-game-list-item/video-game-list-item.component";
-import {ModifygamesComponent} from "./app/modifygames/modifygames.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {provideRouter,Routes} from "@angular/router";
+import{VideoGameListComponent} from "./app/video-game-list/video-game-list.component";
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/VideoGames', pathMatch: 'full'},
   {path: 'VideoGames', component: VideoGameListComponent},
-  {path: 'VideoGames/:id', component: VideoGameListItemComponent},
-  {path: 'modifygames', component: ModifygamesComponent},
-  {path: '**', component:PageNotFoundComponent}
+  {path: 'VideoGames/:id',
+  loadComponent: () =>
+  import('./app/video-game-list/video-game-list.component').then(m=> m.VideoGameListComponent)},
+  {path: 'modifygames',
+  loadComponent: () =>
+  import('./app/modifygames/modifygames.component').then(m=>m.ModifygamesComponent)},
+  {path: '**',
+  loadComponent: () =>
+  import('./app/page-not-found/page-not-found.component').then(m=>m.PageNotFoundComponent)}
+
 ];
 
 
-
-bootstrapApplication(AppComponent, {
+bootstrapApplication(AppComponent,{
   providers: [provideRouter(routes)]
-}).then(r => console.log('Bootstrap Successful'))
+}).then(r => console.log("Successful "))
